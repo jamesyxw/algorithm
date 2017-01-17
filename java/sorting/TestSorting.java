eac2465e41fdb8f1a24015de0eb970e61d7b80a0
James Wang <james.wang2016@gmail.com>
2017-01-16 22:40:18 -0800

    Add bubbleSort and more testing

--- java/sorting/TestSorting.java
+++ java/sorting/TestSorting.java
@@ -2,29 +2,50 @@ import java.util.*;
 
 public class TestSorting {
     public static void main(String[] args) {
-        Comparable[] test = generateRandIntArray(10, 20);
-        display(test);
+        run(10);
+    }
 
-        Comparable[] insertionCopy = test.clone();
-        InsertionSort.sort(insertionCopy);
-        display(insertionCopy);
-        System.out.println(validate(insertionCopy));
+    private static void run(int count) {
+        for (int j = 0; j < count; j++) {
+            for (int i = 1; i < 10; i++) {
+                Comparable[] test = generateRandIntArray(i, 20);
+                display(test);
+                runTest(test);
+            }
+        }
+    }
 
-        Comparable[] quickCopy = test.clone();
-        QuickSort.sort(quickCopy);
-        display(quickCopy);
-        System.out.println(validate(quickCopy));
+    private static void runTest(Comparable[] test) {
+        try {
+            Comparable[] insertionCopy = test.clone();
+            InsertionSort.sort(insertionCopy);
+            display(insertionCopy);
+            System.out.println(validate(insertionCopy));
 
-        Comparable[] mergeCopy = test.clone();
-        MergeSort.sort(mergeCopy);
-        display(mergeCopy);
-        System.out.println(validate(mergeCopy));
+            Comparable[] quickCopy = test.clone();
+            QuickSort.sort(quickCopy);
+            display(quickCopy);
+            System.out.println(validate(quickCopy));
 
-        Comparable[] heapSortCopy = test.clone();
-        HeapSort.sort(heapSortCopy);
-        display(heapSortCopy);
-        System.out.println(validate(heapSortCopy));
+            Comparable[] mergeCopy = test.clone();
+            MergeSort.sort(mergeCopy);
+            display(mergeCopy);
+            System.out.println(validate(mergeCopy));
 
+            Comparable[] heapSortCopy = test.clone();
+            HeapSort.sort(heapSortCopy);
+            display(heapSortCopy);
+            System.out.println(validate(heapSortCopy));
+
+            Comparable[] bubbleCopy = test.clone();
+            BubbleSort.sort(bubbleCopy);
+            display(bubbleCopy);
+            System.out.println(validate(bubbleCopy));
+            
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        
     }
 
     public static void display(Comparable[] a){
@@ -34,10 +55,10 @@ public class TestSorting {
         System.out.println();
     }
 
-	private static boolean validate(Comparable[] array) {
+	private static boolean validate(Comparable[] array) throws Exception{
 		for (int i = 1; i < array.length; i++) {
 			if (array[i].compareTo(array[i-1]) < 0) {
-				return false;
+				throw new Exception("Wrong results");
 			}
 		}
 		return true;
