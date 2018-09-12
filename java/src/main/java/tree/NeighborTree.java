e03f3f9aea754496b62db4fdd81627b211a873e0
jamesyxw <james.wang2016@gmail.com>
2018-09-11 22:18:45 -0700

    Neighbor tree

---
+++ java/src/main/java/tree/NeighborTree.java
@@ -0,0 +1,16 @@
+package tree;
+
+public class NeighborTree {
+	
+	public BSTNode root;
+	
+	public static void bfs(BSTNode root) {
+		
+	}
+	
+	public class BSTNode {
+		BSTNode left;
+		BSTNode right;
+		BSTNode next;
+	}
+}
