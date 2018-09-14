9292618ade2d99cdaf2438ba00fbc4c1258b34da
jamesyxw <james.wang2016@gmail.com>
2018-09-13 23:30:02 -0700

    add MaxBinaryTree, ReserveLinkedList, ReverseWordsInAStringII RomanToInteger to be finished

---
+++ java/src/main/java/puzzle/lintcode/microsoft/ReverseLinkedList.java
@@ -0,0 +1,50 @@
+package puzzle.lintcode.microsoft;
+
+import puzzle.lintcode.common.LinkedListUtils;
+import puzzle.lintcode.common.ListNode;
+
+/**
+ * Reverse a linked list.
+ * For linked list 1->2->3, the reversed linked list is 3->2->1
+ * 
+ * Reverse it in-place and in one-pass
+ * 
+ * @author jamesyxw
+ *
+ */
+public class ReverseLinkedList {
+	
+	public static void main(String[] args) {
+		ListNode l1 = LinkedListUtils.buildList(1,2,3,4);
+		LinkedListUtils.displayLinkedList(l1);
+		ListNode l1Reversed = reverse(l1);
+		LinkedListUtils.displayLinkedList(l1Reversed);
+		
+	}
+
+	/**
+     * @param head: n
+     * @return: The new head of reversed linked list.
+     */
+    public static ListNode reverse(ListNode head) {
+    	if (head == null || head.next == null) {
+    		return head;
+    	}
+    	
+        ListNode current = head;
+        ListNode prev = null;
+        ListNode next = head.next;
+        
+        
+        while (next != null) {
+        	current.next = prev;
+        	prev = current;
+        	current = next;
+        	next = next.next;
+        }
+        
+        current.next = prev;
+        
+        return current;
+    }
+}
---
+++ java/src/main/java/puzzle/lintcode/microsoft/RomanToInteger.java
@@ -0,0 +1,32 @@
+package puzzle.lintcode.microsoft;
+
+/**
+ * Given a roman numeral, convert it to an integer.
+
+	The answer is guaranteed to be within the range from 1 to 3999.
+	
+	IV -> 4
+
+	XII -> 12
+	
+	XXI -> 21
+	
+	XCIX -> 99
+
+ * @author jamesyxw
+ *
+ */
+public class RomanToInteger {
+
+	/**
+     * @param s: Roman representation
+     * @return: an integer
+     */
+    public int romanToInt(String s) {
+        // write your code here
+    	int result = 0;
+    	
+    	return result;
+    	
+    }
+}
